08edae92cdc8c374031ecca1bec6d628118b0932
gmedinav <gmmedinavelazco@gmail.com>
2017-08-11 01:47:53 -0500

    ok
    migrado

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8081/ci_casa_oficios/';
+$config['base_url'] = 'http://localhost:8081/proyecto_casa_oficios/';
 
 /*
 |--------------------------------------------------------------------------
