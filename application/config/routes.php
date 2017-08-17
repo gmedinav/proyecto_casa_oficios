f1900a077815951c3b5b7417e0a680f044dcc74b
jaimeac882 <u21215428@upc.edu.pe>
2017-08-16 22:50:45 -0500

    Cambios para Subir RestFull
    RestFull

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+//Rutas de examples
+$route['cities']['get'] = 'cities/index';
+$route['cities/(:num)']['get'] = 'cities/find/$1';
+$route['cities']['post'] = 'cities/index';
+$route['cities/(:num)']['put'] = 'cities/index/$1';
+$route['cities/(:num)']['delete'] = 'cities/index/$1';
+
+$route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
+$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
+//
