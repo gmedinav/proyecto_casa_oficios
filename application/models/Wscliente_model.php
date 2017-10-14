8af172ff5434247fa8ea778045a3e09fa1bb99da
jaimeac882 <u21215428@upc.edu.pe>
2017-10-14 01:17:03 -0500

    Ws Insertar Cliente
    Ws Insertar Cliente   - Guardados en Firefox

--- application/models/Wscliente_model.php
+++ application/models/Wscliente_model.php
@@ -20,6 +20,99 @@ class Wscliente_model extends CI_Model{
         parent::__construct();
     }
     
+    
+    private $contactxx = 'contact';
+    
+    
+    function add_contact($contact_name, $contact_address, $contact_phone) {
+        echo $contact_name;
+        $data = array('contact_name' => $contact_name, 'contact_address' => $contact_address, 'contact_phone' => $contact_phone);
+        $this->db->insert($this->contactxx, $data);
+    }
+    
+    
+     public   function add_cliusu($p_log_usuario ,
+                        $p_pass_usuario ,
+                        $p_estado ,
+                        $p_cod_tipo_usuario ,
+                        $p_fec_registro ,
+                        $p_fec_modificacion,
+                        $p_cod_usuario_registro,
+                        $p_nom_cliente ,
+                        $p_ape_paterno ,
+                        $p_ape_materno ,
+                        $p_cod_tipo_documento ,
+                        $p_num_documento ,
+                        $p_cod_tipo_genero,
+                        $p_cod_ubigeo ,
+                        $p_direccion ,
+                        $p_cel_1 ,
+                        $p_cel_2 ,
+                        $p_cuenta_facebook ,
+                        $p_cuenta_gmail ,
+                        $p_fecha_nacimiento,
+                        $p_cod_tipo_canal_contacto ,
+                        $p_fec_modificacion_cli ,
+                        $p_fec_registro_cli ,
+                        $p_cod_usuario_registro_cli ,
+                        $p_estado_cli ){
+//        echo $contact_name;
+//        $data = array('contact_name' => $contact_name, 'contact_address' => $contact_address, 'contact_phone' => $contact_phone);
+//        $this->db->insert($this->contact, $data);
+//        return $data;
+//         $this->load->database();
+//          $sql = "call sp_cli";
+//           $query = $this->db->query($sql);
+//          return $query->result_array();
+          
+         
+         $out_id = 0;
+          
+            $call_procedure = "CALL sp_insert_cli_usu('"
+                    .$p_log_usuario."'".
+                    ",'".$p_pass_usuario."'".
+                    "," .$p_estado.
+                    "," .$p_cod_tipo_usuario.
+                    ",'" .$p_fec_registro."'".
+                    ",'" .$p_fec_modificacion."'".
+                    "," .$p_cod_usuario_registro.
+                    ",'" .$p_nom_cliente."'".
+                    ",'" .$p_ape_paterno."'".
+                    ",'" .$p_ape_materno."'".
+                    "," .$p_cod_tipo_documento.
+                    ",'" .$p_num_documento."'".
+                    "," .$p_cod_tipo_genero.
+                    ",'" .$p_cod_ubigeo."'".
+                    ",'" .$p_direccion."'".
+                    ",'" .$p_cel_1."'".
+                    ",'" .$p_cel_2."'".
+                    ",'" .$p_cuenta_facebook."'".
+                    ",'" .$p_cuenta_gmail."'".
+                    ",'" .$p_fecha_nacimiento."'".
+                    "," .$p_cod_tipo_canal_contacto.
+                    ",'" .$p_fec_modificacion_cli."'".
+                    ",'" .$p_fec_registro_cli."'".
+                    "," .$p_cod_usuario_registro_cli.
+                    "," .$p_estado_cli.");";
+//                    ",@out_id);";
+             
+            
+            
+            
+            //echo $call_procedure;
+            
+            
+         $query =   $this->db->query($call_procedure);
+            
+   //         $call_total = 'SELECT @out_id as codigo';
+//            
+       //    $query = $this->db->query($call_total);
+           // return $query->result();
+          return $query->result_array();
+         
+    }
+    
+    
     public function  getCliente($codigo){
              
           $this->load->database();
