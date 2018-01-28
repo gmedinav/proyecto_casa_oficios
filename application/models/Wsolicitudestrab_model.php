92bf373119096d37f4c1022811c528164cccb858
jaimeac882 <u21215428@upc.edu.pe>
2018-01-28 13:18:35 -0500

    WS_Trab_Solic
    Proyecto Trab SOlicitudes

--- application/models/Wsolicitudestrab_model.php
+++ application/models/Wsolicitudestrab_model.php
@@ -24,11 +24,11 @@ class Wsolicitudestrab_model extends CI_Model{
              
           $this->load->database();
           
-           $sql = "SELECT  C.COD_SOLICITUD AS COD_SOLICITUD, C.DESCRIPCION AS DESCRIPCION , C.FEC_REGISTRO AS FEC_REGISTRO ,
-                        F.DESCRIPCION AS DESESTADO
-                         FROM TB_SOLICITUD_TRABAJO C INNER JOIN TB_ESTADO_SOLICITUD_TRABAJO F
-                        ON C.ESTADO = F.COD_ESTADO
-                        WHERE COD_USUARIO_REGISTRO=".$codigo.'';
+           $sql = "select  c.cod_solicitud as cod_solicitud, c.descripcion as descripcion , c.fec_registro as fec_registro ,
+                        f.descripcion as desestado
+                         from tb_solicitud_trabajo c inner join tb_estado_solicitud_trabajo f
+                        on c.estado = f.cod_estado
+                        where cod_usuario_registro=".$codigo.'';
           
           $query = $this->db->query($sql);
           
